a8de0876e05361e685a293972d7508ce3bdf91eb
SuperMusey <rishavde.1729@gmail.com>
2022-06-01 22:32:10 -0400

    Update bigdata.js

--- src_code/bigdata.js
+++ src_code/bigdata.js
@@ -20,57 +20,13 @@ dataMap.set("ENGEC411",[{prof: "Scrodinger",rating: 4.2,starttime:1050,endtime:1
                         {prof: "Maria",rating: 2.2,starttime:1730,endtime:1810,days:["M","F"]}]);
 dataMap.set("CASPS251",[{prof:"Shim",rating: 4.8,starttime:1830,endtime:2100,days:["M","W"]}]);
 
-
-// const college_arr = ["ENGEC327",
-//                      "CASPY212", 
-//                      "CASPY212", 
-//                      "CASPY212", 
-//                      "CASMA225", 
-//                      "CASMA225", 
-//                      "CASMA225", 
-//                      "CASMA225",
-//                      "ENGEC311",
-//                      "ENGEC411",
-//                      "ENGEC411",
-//                      "CASPS215"];
-// const prof_arr =    ["Densmore",
-//                      "Butler",
-//                      "Hu",
-//                      "Suarez",
-//                      "Lin",
-//                      "Divac",
-//                      "Sheshmani",
-//                      "Weinstein",
-//                      "Joshi",
-//                      "Shien",
-//                      "Scrodinger",
-//                      "Diluc"];
-// const rating =      [3.8,4.5,1.0,2.5,4.1,3.3,0,3.9,3.3,3.6,4.2,4.5];
-// const class_start_time = [1430,930,1400,1700,1010,1230,905,1325,1220,1100,1200,1600];
-// const class_end_time = [1615,1045,1515,1815,1100,1345,955,1415,1405,1330,1250,1730];
-// const days = ["MW","TR","TR","TR","MWF","TR","MWF","MWF","MW","TRF","MWF","MW"];
-
-// const test_data = [1,2];
-
-// function makeData(college,dept,course,prof,rating,starttime,endtime,days){
-//     var dataObj = {
-//         college: college,
-//         dept: dept,
-//         course: course,
-//         prof:prof,
-//         rating: rating,
-//         starttime: starttime,
-//         endtime: endtime,
-//         days: days
-//     };
-//     return dataObj;
-// }
-// var data_of_courses = [];
-// function returndata(){
-//     data_of_courses.push(makeData(ENG,EC,311,Lin,3.6,1300,1440,["M","W","F"]));
-//     data_of_courses.push(makeData(ENG,EK,330,TallBoi,4.6,1200,1255,["T","R","F"]));
-//     return data_of_courses;
-// }
-
-//pushes dont work
+//For Data stucture Team READ:
+//each dataMap.set() contains an array of objects where each object is a lecture of the course key
+//Example: dataMap.set("CASPS251",[{prof:"Shim",rating: 4.8,starttime:1830,endtime:2100,days:["M","W"]}]);
+//course key is CASPS251 and the array contains objects (this contains only 1) which is the lecture
+//THE STRUCTURE OF OUTPUT:
+//for each class do:
+//dataMap.set("<course>",[n# of lectures of <course> in objects {} seperated by ,])
+//in each object it is {prof:"<NAME",rating:NUMBER,starttime:NUMBER,endtime:NUMBER,days:['day1','day2',etc]}
+//so a course with 3 lectures would have 3 such objects in the array of size 3 seperated by ,'s
 
